569d43c21c3e0c3e8868114b0b946d5489495da9
arparvej1 <parvej8pro@gmail.com>
2024-04-26 19:59:41 +0600

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.MY_KEY)
 
 app.get('/',  (req, res) => {
   res.send('Server is running...')
